87890c7fafc04d1ed22d968b5962c6fa5912b06b
fant2477 <fant_2477@hotmail.com>
2021-04-21 16:18:46 +0700

    Update Workshop3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import { Component } from "react";
 import Clock from "./components/Clock";
+import Form from "./components/Form";
+import FormCallback from "./components/FormCallback";
 import User from "./components/User";
+import User2 from "./components/User2";
 
 class App extends Component {
+  users = [
+    { id: 1, name: "User 01", age: 10 },
+    { id: 2, name: "User 02", age: 20 },
+  ];
+
+  constructor() {
+    super();
+    this.state = { message: "-------" };
+    this.xyz = this.xyz.bind(this);
+  }
+
+  // Callback
+  xyz(input) {
+    this.setState({ message: "New section : " + input });
+  }
+
   render() {
     return (
       <div>
@@ -11,7 +30,29 @@ class App extends Component {
           <User info="User 02" />
           <User info="User 03" />
         </ul>
+        
+        <hr/>
+        <br/>
+
+        <User2 users={this.users} />
+        
+        <hr/>
+        <br/>
+
         <Clock title="Current data naja = " />
+        
+        <hr/>
+        <br/>
+        
+        <p>Form: </p>
+        <Form />
+        
+        <hr/>
+        <br/>
+
+        <p>FormCallback: </p>
+        <FormCallback onSayHi={this.xyz} />
+        <p>{this.state.message}</p>
       </div>
     );
   }
